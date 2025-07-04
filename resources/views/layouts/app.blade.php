51b3393daeb6055dfc67642c1e84df3d558e4fc6
rnik82 <rnik82@mail.ru>
2025-07-04 11:33:54 +0300

    retern hexlet assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,12 +13,17 @@
     <title>{{ __('messages.task_manager') }}</title>
 
     <!-- Подключение Vite -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 
-    <!-- Scripts -->
+   <!-- Scripts -->
     <link rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/rails-ujs@5.2.8-1/lib/assets/compiled/rails-ujs.min.js"></script>
+
+    <link rel="preload" as="style" href="https://php-task-manager-ru.hexlet.app/build/assets/app.4885a691.css" />
+    <link rel="modulepreload" href="https://php-task-manager-ru.hexlet.app/build/assets/app.42df0f0d.js" />
+    <link rel="stylesheet" href="https://php-task-manager-ru.hexlet.app/build/assets/app.4885a691.css" />
+    <script type="module" src="https://php-task-manager-ru.hexlet.app/build/assets/app.42df0f0d.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
