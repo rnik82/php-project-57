84f972d09579af7ba0af56f53d49c5bbd55de92c
rnik82 <rnik82@mail.ru>
2025-07-08 12:13:15 +0300

    add debug Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,5 +20,3 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         Integration::handles($exceptions);
     })->create();
-
-
